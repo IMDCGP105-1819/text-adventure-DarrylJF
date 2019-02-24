97d6c84ec3c27d41527f618e8107ed18177e32c9
DarrylJF <darryl.jade.frost@gmail.com>
2019-02-24 12:05:51 +0000

    attempted game loop but the game ending depends on all rooms 'visited' attr being ==TRUE so needs to be changed. attempted intro text to accept user name from input and initialise player but think this will need to be direct method on player class

---
+++ game.py
@@ -0,0 +1,13 @@
+from player import Player
+from rooms import Room
+
+
+def intro(name):
+    name = input("Enter your name: ")
+    PLAYER1 = Player(name,hp,)
+    
+
+def Main():
+    gameOver = False
+    while gameOver == False:
+        pass
