2ab8fd31676711d3cf95eed6dbbfda13d689b694
DarrylJF <darryl.jade.frost@gmail.com>
2019-02-27 00:33:30 +0000

    changed items added parent classes of Weapon and Heals so the subclasses can inherit __str__ and methods directly from parent. Cant figure out how to use objects interactively

--- items.py
+++ items.py
@@ -1,48 +1,65 @@
-"""
-Items will be (classed as loot)
-
-Weapons: Knife, Gun, Rock 
-
-Backpack: Level 1 = 4 slots, Level 2 = 5 slots.
-
-Level 2 backpack in first room but have to kill enemy in room 1 to 
-be able to equip. However there is no weapons in room 1 so only if 
-pickup rock from outside can you kill room enemy if do not pickup 
-backpack will not be able to carry items in inventory.
-
-Will give hints to pickup rock before entering first room and advise to equip a backpack.
-
-Items will have associated actions player can invoke.
-
-"""
-from player import Player
-
-class Items:
-    def __init__(self, name, description, value, weight):
+class Weapon:
+    def __init__(self, name, description, weight, damage):
         self.name = name
         self.description = description
-        self.value = value
         self.weight = weight
+        self.damage = damage
     
     def __str__(self):
-        return f'Name: {self.name}\nDescription: {self.description}\nValue: {self.value}\nWeight: {self.weight}'
+        return f"\nName: {self.name}\nDescription: {self.description}\nWeight: {self.weight}\nDamage: {self.damage}"
 
-AK47 = Items("AK-47", "The AK-47, officially know as the Avtomat Kalashnikova. Automatic Rifle is a gas-operated, 7.62x39mm assault rifle", 100, 3)
-SHANK = Items("Shank", "Homemade knife like weapon, especially one fashioned in prison. The related verb shiv means 'To stab someone'", 50, 1)
-ROCK = Items("Rock", "Literally a rock. Pickup a rock, take no room in inventory you can keep it in ya pocket!", 100, 0)
+class Knife(Weapon):
+    def __init__(self):
+        self.name = "Knife"
+        self.description = "Shank, effective close range"
+        self.weight = 1
+        self.damage = 50
 
+class AK47(Weapon):
+    def __init__(self):
+        self.name = "AK-47"
+        self.description = "Also known as the Kalashnikov"
+        self.weight = 2
+        self.damage = 100
 
+class Rock(Weapon):
+    def __init__(self):
+        self.name = "Rock"
+        self.description = "By any means necessary"
+        self.weight = 0
+        self.damage = 25
 
-class Backpack:
-    def __init__(self, name, level, slots):
+class Heals:
+    def __init__(self, name, weight, value):
         self.name = name
-        self.level = level
-        self.slots = slots
-
+        self.weight = weight
+        self.value = value
+    
     def __str__(self):
-        return f'Name: {self.name}\nLevel: {self.level}\nSlots: {self.slots}'
+        return f"\nName: {self.name}\nWeight: {self.weight}\nHeal Value: {self.value}"
+
+class Bread(Heals):
+    def __init__(self):
+        self.name = "Bread"
+        self.weight = 0
+        self.value = 10
+
+class Bandage(Heals):
+    def __init(self):
+        self.name = "Bandage"
+        self.weight = 0
+        self.value = 25
+
+class MedKit(Heals):
+    def __init__(self):
+        self.name = "Med Kit"
+        self.weight = 1
+        self.value = 100
 
 
-BACKPACK1 = Backpack("Standard", "1", 2)
+
+
+
+
 
 
