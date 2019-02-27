0d41f13db2967c929b0e4384b6da1d38294c5c02
DarrylJF <darryl.jade.frost@gmail.com>
2019-02-27 00:23:01 +0000

    enemies created with corosponding room names for later x,y placement. cant figure out how to map enemies to a room map location using MapTile

--- enemies.py
+++ enemies.py
@@ -1,21 +1,35 @@
-
-"""
-Enemies will have hp value which will be attached to a player attack function where it the game
-will suggest best weapon of attack based on enemy hp and weapon damage if player has in there
-inventory. 
-
-If weapons in inventory dont have value game will tell you unable to kill with current
-arsenal and suggest next best weapon.
-
-If you have to use two attacks to kill enemy player will have to take a blow and player hp will be
-checked and if hp < enemy attack value you will not be able to attack unless you heal to a > hp first
-
-"""
-from player import Player
-
 class Enemy:
-    def __init__(self, name, description, hp, damage):
+    def __init__(self, name, hp, attack):
         self.name = name
-        self.description = ""
         self.hp = hp
-        self.damage = damage
+        self.attack = attack
+
+    def __str__(self):
+        return f"\nName: {self.name}\nHP: {self.hp}\nAttack: {self.attack}"
+
+    def dead_or_alive(self):
+        return self.hp > 0
+
+class LootRoomSquatter(Enemy):
+    def __init__(self):
+        self.name = "Squatter"
+        self.hp = 100
+        self.attack = 0
+
+class Room1Enemy(Enemy):
+    def __init__(self):
+        self.name = "Ninja"
+        self.hp = 1000 # obviously 1000 hp its a ninja im tryna make sure you dont die
+        self.attack = 50
+
+class Room2Enemy(Enemy):
+    def __init__(self):
+        self.name = "John has a gun, but hes slow"
+        self.hp = 100
+        self.attack = 50
+
+
+
+    
+
+        
